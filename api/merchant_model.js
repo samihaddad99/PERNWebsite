f6b69358222f5731e13379cc331717c668abfa30
Sami Haddad <77640501+samihaddad99@users.noreply.github.com>
2023-02-03 15:28:54 -0500

    updated DATABASE_URL

--- api/merchant_model.js
+++ api/merchant_model.js
@@ -16,14 +16,18 @@ const { Pool } = require('pg')
 
 // heroku pool
 const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-const pool = new Pool({
-  connectionString: isProduction
-    ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
-    : connectionString,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
-});
+// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const connectionString = $DATABASE_URL;
+// const pool = new Pool({
+//   connectionString: isProduction
+//     ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
+//     : connectionString,
+//   ssl: isProduction ? { rejectUnauthorized: false } : false,
+// });
 
+const pool = new Pool({
+  connectionString
+})
 // local pool
 // const pool = new Pool({
 //   user: 'nglyfwwl',
