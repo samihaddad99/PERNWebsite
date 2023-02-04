f5b5ec017040a3634a12e3b24a8eb85e377f2ad8
Sami Haddad <77640501+samihaddad99@users.noreply.github.com>
2023-02-04 11:59:49 -0500

    changed some backend credentials

--- api/merchant_model.js
+++ api/merchant_model.js
@@ -15,15 +15,15 @@ const { Pool } = require('pg')
 // });
 
 // heroku pool
-// const isProduction = process.env.NODE_ENV === "production";
-// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 // const connectionString = $DATABASE_URL;
-// const pool = new Pool({
-//   connectionString: isProduction
-//     ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
-//     : connectionString,
-//   ssl: isProduction ? { rejectUnauthorized: false } : false,
-// });
+const pool = new Pool({
+  connectionString: isProduction
+    ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
+    : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
 
 // const pool = new Pool({
 //   "postgres://nglyfwwl":"VgEsZXQmoxonCtdAw9xXgMNEJdeeLBQt@heffalump.db.elephantsql.com/nglyfwwl"
