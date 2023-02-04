3bd54cdad579339c3eab630c7dfbec5e85c51595
Sami Haddad <77640501+samihaddad99@users.noreply.github.com>
2023-02-04 11:47:14 -0500

    backend changes/cors enabled

--- api/merchant_model.js
+++ api/merchant_model.js
@@ -5,14 +5,14 @@
 const { Pool } = require('pg')
 
 // heroku pool
-const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-const pool = new Pool({
-  connectionString: isProduction
-    ? process.env.$(DATABASE_URL) // Heroku will supply us with a string called DATABASE_URL for the connectionString,
-    : connectionString,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
-});
+// const isProduction = process.env.NODE_ENV === "production";
+// const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+// const pool = new Pool({
+//   connectionString: isProduction
+//     ? process.env.$(DATABASE_URL) // Heroku will supply us with a string called DATABASE_URL for the connectionString,
+//     : connectionString,
+//   ssl: isProduction ? { rejectUnauthorized: false } : false,
+// });
 
 // heroku pool
 // const isProduction = process.env.NODE_ENV === "production";
--- api/routes/index.js
+++ api/routes/index.js
@@ -21,6 +21,19 @@ app.use(cors());
 //   next();
 // });
 
+const allowedOrigins = ['https://pernstack-backend.herokuapp.com/',
+                      'https://sami-haddad.netlify.app'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app
 .route("/")
 .get(async function(req, res, next) {
